9a73be45fe302c40a64f9321ee400932fd4ec228
immi <iafullprogrammer@gmail.com>
2025-10-31 14:11:12 +0500

    .

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -2,9 +2,9 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
     entry: ["src/index.ts"],
-    format: ["esm", "cjs"],
+    format: ["esm"],
     dts: true,
     splitting: false,
-    sourcemap: true,
+    sourcemap: false,
     clean: true,
 });
